085f21ef11ac708aecb6d4628e4011669ab49ad8
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2019-03-09 08:33:43 +0200

    better angular form

--- src/app/project-structure/root/components/basic-angular-form/basic-angular-form.component.ts
+++ src/app/project-structure/root/components/basic-angular-form/basic-angular-form.component.ts
@@ -44,7 +44,8 @@ import {
   FormArray,
   FormControl,
   FormGroup,
-  Validators
+  Validators,
+  AbstractControl
 } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
@@ -69,9 +70,16 @@ export class BasicAngularFormComponent implements OnInit, OnDestroy {
  *    │  │ ││││└─┐ │ ├┬┘│ ││   │ │ │├┬┘
  *    └─┘└─┘┘└┘└─┘ ┴ ┴└─└─┘└─┘ ┴ └─┘┴└─
  */
+  private _forbiddenEmails: Array<string> = [
+    'test@test.com',
+    'admin@test.com',
+    'admin@admin.com',
+    'test@admin.com'
+  ]
   public constructor() {
     this.subscriptions = [];
   }
+
 
 
  /***
@@ -101,7 +109,7 @@ export class BasicAngularFormComponent implements OnInit, OnDestroy {
       'email': new FormControl(null, [
         Validators.required,
         Validators.email
-      ]),
+      ], this.validEmailAsync.bind(this)),
       'birthDate': new FormControl(null, Validators.required),
       'gender': new FormControl(null, Validators.required),
       'description': new FormControl(null),
@@ -131,15 +139,31 @@ export class BasicAngularFormComponent implements OnInit, OnDestroy {
  *    │││├┤  │ ├─┤│ │ ││└─┐
  *    ┴ ┴└─┘ ┴ ┴ ┴└─┘─┴┘└─┘
  */
+  private validateAllFormFields(formGroup: FormGroup | FormArray) {
+
+    Object.keys(formGroup.controls).forEach(field => {
+      const control: AbstractControl = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      }
+      if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+      if (control instanceof FormArray) {
+        this.validateAllFormFields(control);
+      }
+  });
+}
+
   public onSubmit(): void {
     if (this.userDetailsForm.valid) {
       setTimeout(() => {
         this.submittedMessage = false;
       }, 4000);
       this.submittedMessage = true;
-      this.userDetailsForm.reset();
       console.log(this.userDetailsForm.value);
-    }
+      this.userDetailsForm.reset();
+    } else this.validateAllFormFields(this.userDetailsForm);
   }
 
   public onAddHobbyInput(): void {
@@ -158,6 +182,22 @@ export class BasicAngularFormComponent implements OnInit, OnDestroy {
       return { 'usernameForbiddenCharacter': true };
     }
     return null;
+  }
+
+  public validEmailAsync(control: FormControl): Promise<{ [key: string]: boolean }> {
+    return new Promise<{ [key: string]: boolean }>((resolve, reject) => {
+      setTimeout(() => {
+        (this._forbiddenEmails.indexOf(control.value) >= 0 ) ? resolve({ 'emailIsForbidden': true }) : resolve(null);
+      }, 300);
+    });
+  }
+
+  public emailInputError(): 'required' | 'emailIsForbidden' | 'email' | null {
+    if (this.userDetailsForm.get('email').invalid) {
+      if (this.userDetailsForm.get('email').errors.required) return 'required';
+      if (this.userDetailsForm.get('email').errors.email) return 'email';
+      if (this.userDetailsForm.get('email').errors.emailIsForbidden) return 'emailIsForbidden';
+    } else return null;
   }
 
 }
